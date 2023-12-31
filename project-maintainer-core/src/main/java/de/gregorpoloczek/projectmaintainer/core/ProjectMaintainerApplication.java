9f6d89a2668b5a7ecce392817686c5d2e8cf3da4
gpoloczek <gregor.poloczek@immowelt.de>
2023-12-31 13:40:46 +0100

    Setting up maven

---
+++ project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/ProjectMaintainerApplication.java
@@ -0,0 +1,12 @@
+package de.gregorpoloczek.projectmaintainer.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class ProjectMaintainerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectMaintainerApplication.class, args);
+  }
+}
