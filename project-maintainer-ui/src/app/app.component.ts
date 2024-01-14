f98e290a0bd238b8eadc9f39865b8df9c27d9ec2
gpoloczek <gregor.poloczek@immowelt.de>
2024-01-14 19:11:37 +0100

    Data abstraction

--- project-maintainer-ui/src/app/app.component.ts
+++ project-maintainer-ui/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import axios from 'axios';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import moment from 'moment';
 
 interface ProjectResource {
   fqpn: string;
@@ -23,22 +25,43 @@ interface ProjectResource {
   };
 }
 
+class ProjectListItem {
+  name: string;
+  commit: string | null;
+
+  constructor(private projectResource: ProjectResource) {
+    this.name = projectResource.metaData.name;
+    let latestCommit = projectResource.git.workingCopy?.latestCommit;
+    if (latestCommit) {
+      this.commit = `${moment(latestCommit.timestamp).fromNow()} - ${latestCommit.message}`;
+    } else {
+      this.commit = null;
+    }
+  }
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatListModule],
+  imports: [CommonModule, RouterOutlet, MatListModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'project-maintainer-ui';
-  projects: ProjectResource[] = [];
+  projects: ProjectListItem[] = [];
 
   ngOnInit() {
-    this.getProjects().then((r) => (this.projects = r.data));
+    (async () => {
+      this.projects = await this.getProjects();
+    })();
   }
 
-  getProjects() {
-    return axios.get<ProjectResource[]>('http://localhost:8080/v1/projects/');
+  async getProjects(): Promise<ProjectListItem[]> {
+    const projects = await axios
+      .get<ProjectResource[]>('http://localhost:8080/v1/projects/')
+      .then((r) => r.data);
+
+    return projects.map((p) => new ProjectListItem(p));
   }
 }
