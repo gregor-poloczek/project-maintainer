e7c4a90110e89e22a568b3f36d4a4a875e65b029
Gregor Poloczek <gregor.poloczek@cb-gmbh.com>
2024-05-20 18:45:57 +0200

    adding pulling of projects via UI

---
+++ project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/common/ui/ImageResolverService.java
@@ -0,0 +1,55 @@
+package de.gregorpoloczek.projectmaintainer.core.common.ui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+import lombok.Builder;
+import lombok.Getter;
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageResolverService {
+
+    @Builder
+    @Getter
+    public static class ImageFormat {
+
+        private String extension;
+        private String mimetype;
+    }
+
+    @Builder
+    @Getter
+    public static class Image {
+
+        private ImageFormat format;
+        private byte[] bytes;
+    }
+
+    public Optional<Image> getImage(String path, String name) {
+        return Stream.of(
+                        ImageFormat.builder().extension("svg").mimetype("image/svg+xml").build(),
+                        ImageFormat.builder().extension("png").mimetype("image/x-png").build())
+                .map(format -> {
+                    URL resource = this.getClass().getClassLoader().getResource(
+                            "de/gregorpoloczek/projectmaintainer/core/%s/%s.%s".formatted(path,
+                                    name, format.getExtension()));
+                    if (resource == null) {
+                        return null;
+                    }
+                    byte[] bytes;
+                    try {
+                        bytes = IOUtils.toByteArray(resource);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    return Image.builder().format(format).bytes(bytes).build();
+                })
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+}
--- project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/common/ui/MainView.java
+++ project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/common/ui/MainView.java
@@ -1,30 +1,20 @@
 package de.gregorpoloczek.projectmaintainer.core.common.ui;
 
 import com.vaadin.flow.component.AttachEvent;
-import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.Grid.SelectionMode;
+import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.renderer.LitRenderer;
 import com.vaadin.flow.router.Route;
-import de.gregorpoloczek.projectmaintainer.core.domain.git.service.WorkingCopyService;
-import de.gregorpoloczek.projectmaintainer.core.domain.project.api.resources.GitProvider;
+import de.gregorpoloczek.projectmaintainer.core.common.ui.ImageResolverService.Image;
+import de.gregorpoloczek.projectmaintainer.core.domain.communication.service.OperationExecutionService;
 import de.gregorpoloczek.projectmaintainer.core.domain.project.service.ProjectService;
 import de.gregorpoloczek.projectmaintainer.core.domain.project.service.dtos.Project;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.net.URL;
 import java.util.Base64;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Stream;
-import javax.swing.text.html.Option;
-import lombok.Builder;
-import lombok.Getter;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.commons.lang3.tuple.Triple;
+import reactor.core.Disposable;
 
 @Route
 public class MainView extends VerticalLayout {
@@ -32,7 +22,7 @@ public class MainView extends VerticalLayout {
     public final LitRenderer<Project> iconRenderer =
             LitRenderer.<Project>of("<img src=${item.image} style=\"height:32px;\" />")
                     .withProperty("image", project -> {
-                        Optional<Image> image = MainView.this.getImage("gitprovider",
+                        Optional<Image> image = MainView.this.imageResolverService.getImage("gitprovider",
                                 project.getMetaData().getGitProvider()
                                         .name());
 
@@ -41,60 +31,63 @@ public class MainView extends VerticalLayout {
                     });
 
 
-    @Builder
-    @Getter
-    private static class ImageFormat {
-
-        private String extension;
-        private String mimetype;
-    }
-
-    @Builder
-    @Getter
-    private static class Image {
-
-        private ImageFormat format;
-        private byte[] bytes;
-    }
-
-    private Optional<Image> getImage(String path, String name) {
-        return Stream.of(
-                        ImageFormat.builder().extension("svg").mimetype("image/svg+xml").build(),
-                        ImageFormat.builder().extension("png").mimetype("image/x-png").build())
-                .map(format -> {
-                    URL resource = this.getClass().getClassLoader().getResource(
-                            "de/gregorpoloczek/projectmaintainer/core/%s/%s.%s".formatted(path,
-                                    name, format.getExtension()));
-                    if (resource == null) {
-                        return null;
-                    }
-                    byte[] bytes;
-                    try {
-                        bytes = IOUtils.toByteArray(resource);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                    return Image.builder().format(format).bytes(bytes).build();
-                })
-                .filter(Objects::nonNull)
-                .findFirst();
-    }
-
     private final ProjectService projectService;
+    private final OperationExecutionService operationExecutionService;
+    private final ImageResolverService imageResolverService;
     private final Grid<Project> grid;
+    private Disposable subscription;
 
-    public MainView(ProjectService projectService) {
+    public MainView(
+            ProjectService projectService,
+            ImageResolverService imageResolverService,
+            OperationExecutionService operationExecutionService) {
         this.projectService = projectService;
+        this.imageResolverService = imageResolverService;
+        this.operationExecutionService = operationExecutionService;
+
         grid = new Grid<>(Project.class, false);
+        grid.setSelectionMode(SelectionMode.MULTI);
         grid.addColumn(this.iconRenderer).setFlexGrow(0);
         grid.addColumn(p -> p.getMetaData().getName()).setHeader("Name");
+
+        MenuBar menuBar = createManuBar();
+
+        this.add(menuBar);
         this.add(grid);
+    }
+
+    private MenuBar createManuBar() {
+        MenuBar menuBar = new MenuBar();
+        menuBar.addItem("Clone / Pull", (e) -> {
+            for (Project project : grid.getSelectionModel().getSelectedItems()) {
+                if (project.isCloned()) {
+                    operationExecutionService.executeAsyncOperation(
+                            project,
+                            "git::pull",
+                            this.projectService::pullProject);
+                }
+            }
+        });
+
+        menuBar.addItem("Wipe", (e) -> {
+            for (Project project : grid.getSelectionModel().getSelectedItems()) {
+                System.out.println(project.getFQPN());
+            }
+        });
+        return menuBar;
     }
 
     @Override
     protected void onAttach(AttachEvent attachEvent) {
         super.onAttach(attachEvent);
+        subscription = this.operationExecutionService.getUpdateEvents().subscribe((e) -> {
+            System.out.println(e);
+        });
         grid.setItems(projectService.getProjects());
+    }
+
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        this.subscription.dispose();
     }
 }
