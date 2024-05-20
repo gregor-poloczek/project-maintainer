f2b880402ffb11a445869e63b271d7a2d23a9f5c
Gregor Poloczek <gregor.poloczek@cb-gmbh.com>
2024-05-20 20:40:45 +0200

    adding progress bars

--- project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/domain/project/service/ProjectOperationState.java
+++ project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/domain/project/service/ProjectOperationState.java
@@ -1,5 +1,15 @@
 package de.gregorpoloczek.projectmaintainer.core.domain.project.service;
 
 public enum ProjectOperationState {
-  SCHEDULED, STARTED, RUNNING, SUCCEEDED, FAILED
+    SCHEDULED(false), STARTED(false), RUNNING(false), SUCCEEDED(true), FAILED(true);
+
+    ProjectOperationState(boolean terminated) {
+        this.terminated = terminated;
+    }
+
+    private final boolean terminated;
+
+    public boolean isTerminated() {
+        return terminated;
+    }
 }
