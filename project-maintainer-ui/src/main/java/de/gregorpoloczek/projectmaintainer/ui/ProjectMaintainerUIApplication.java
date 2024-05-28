61fd3497ffedbb688659799335fc57cce2fc2677
Gregor Poloczek <gregor.poloczek@cb-gmbh.com>
2024-05-28 21:53:39 +0200

    using dark theme

--- project-maintainer-ui/src/main/java/de/gregorpoloczek/projectmaintainer/ui/ProjectMaintainerUIApplication.java
+++ project-maintainer-ui/src/main/java/de/gregorpoloczek/projectmaintainer/ui/ProjectMaintainerUIApplication.java
@@ -2,11 +2,14 @@ package de.gregorpoloczek.projectmaintainer.ui;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @Push
+@Theme(variant = Lumo.DARK)
 @SpringBootApplication(scanBasePackages = "de.gregorpoloczek.projectmaintainer")
 public class ProjectMaintainerUIApplication implements AppShellConfigurator {
 
