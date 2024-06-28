6219a7c8191e5c37eb5b21d4bcd9aca1e6b31ea8
Gregor Poloczek <gregor.poloczek@cb-gmbh.com>
2024-06-28 20:15:56 +0200

    removing obsolete code

--- project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/domain/project/service/ProjectService.java
+++ project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/domain/project/service/ProjectService.java
@@ -1,8 +1,5 @@
 package de.gregorpoloczek.projectmaintainer.core.domain.project.service;
 
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import de.gregorpoloczek.projectmaintainer.core.common.properties.ApplicationProperties;
 import de.gregorpoloczek.projectmaintainer.core.domain.analysis.service.ProjectNotClonedException;
 import de.gregorpoloczek.projectmaintainer.core.domain.git.service.CloneResult;
 import de.gregorpoloczek.projectmaintainer.core.domain.git.service.GitService;
@@ -13,51 +10,32 @@ import de.gregorpoloczek.projectmaintainer.core.domain.project.repository.Projec
 import de.gregorpoloczek.projectmaintainer.core.domain.project.repository.ProjectRepository;
 import de.gregorpoloczek.projectmaintainer.core.domain.project.service.common.FQPN;
 import de.gregorpoloczek.projectmaintainer.core.domain.project.service.dtos.Project;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class ProjectService {
 
-    public static final String PROJECTS_FILE = "projects.json";
-    public static final String SUPPORTED_VERSION = "1";
-    private final ApplicationProperties applicationProperties;
     private final WorkingCopyService workingCopyService;
     private final GitService gitService;
-    private final ObjectMapper objectMapper;
-    private final File projectsDirectory;
     private final ProjectRepository projectRepository;
-    private final File projectsFileRaw;
 
     public List<Project> getProjects() {
         return List.copyOf(this.projectRepository.findAll());
     }
 
     public ProjectService(
-            final ApplicationProperties applicationProperties,
             final WorkingCopyService workingCopyService,
-            final GitService gitService, final ObjectMapper objectMapper,
+            final GitService gitService,
             final ProjectRepository projectRepository
     ) {
-        this.applicationProperties = applicationProperties;
         this.workingCopyService = workingCopyService;
         this.gitService = gitService;
-        this.objectMapper = objectMapper;
-        this.projectsDirectory = applicationProperties.getProjects().getCloneDirectory();
         this.projectRepository = projectRepository;
-        this.projectsFileRaw = new File(projectsDirectory, PROJECTS_FILE);
     }
 
 
@@ -73,7 +51,6 @@ public class ProjectService {
                 workingCopy.getDirectory(),
                 clone.getLatestCommit().orElse(null),
                 workingCopy.getCredentialsProvider()
-
         );
     }
 
--- project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/domain/project/service/dtos/Project.java
+++ project-maintainer-core/src/main/java/de/gregorpoloczek/projectmaintainer/core/domain/project/service/dtos/Project.java
@@ -1,9 +1,5 @@
 package de.gregorpoloczek.projectmaintainer.core.domain.project.service.dtos;
 
-import de.gregorpoloczek.projectmaintainer.core.domain.git.service.Commit;
-import de.gregorpoloczek.projectmaintainer.core.domain.project.service.common.FQPN;
-import de.gregorpoloczek.projectmaintainer.core.domain.project.service.common.Label;
-import java.util.SortedSet;
 import java.util.function.Supplier;
 import org.eclipse.jgit.transport.CredentialsProvider;
 
