f2b0724596ba4a71711556d5bac34b03773516c2
Gregor Poloczek <gregor.poloczek@cb-gmbh.com>
2024-07-01 19:01:01 +0200

    maven analyzer now has more features

--- project-maintainer-ui/src/main/java/de/gregorpoloczek/projectmaintainer/ui/views/reports/ReportsView.java
+++ project-maintainer-ui/src/main/java/de/gregorpoloczek/projectmaintainer/ui/views/reports/ReportsView.java
@@ -1,26 +1,16 @@
 package de.gregorpoloczek.projectmaintainer.ui.views.reports;
 
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteParameters;
-import com.vaadin.flow.router.RouterLink;
-import java.util.List;
 
 @Route("/reports")
 public class ReportsView extends VerticalLayout {
 
-    private final ReportHeader header;
-    private final ReportingProperties reportingProperties;
-
     public ReportsView(
             ReportingProperties reportingProperties) {
-        this.reportingProperties = reportingProperties;
-        this.header = new ReportHeader(reportingProperties);
-        this.header.setTitle("Reports");
-
-        this.add(this.header);
+        ReportHeader header = new ReportHeader(reportingProperties);
+        header.setTitle("Reports");
+        this.add(header);
     }
-
 
 }
