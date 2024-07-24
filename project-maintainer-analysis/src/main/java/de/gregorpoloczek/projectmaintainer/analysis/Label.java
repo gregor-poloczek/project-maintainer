e8b68bc9d8b667c698f4da2106126d020a9bd54f
Gregor Poloczek <gregor.poloczek@cb-gmbh.com>
2024-07-24 21:41:16 +0200

    refactoring

--- project-maintainer-analysis/src/main/java/de/gregorpoloczek/projectmaintainer/analysis/Label.java
+++ project-maintainer-analysis/src/main/java/de/gregorpoloczek/projectmaintainer/analysis/Label.java
@@ -5,20 +5,20 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import lombok.ToString;
 
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
 public class Label implements Comparable<Label> {
 
-    @Getter
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private final String value;
-    @Getter
     private final List<String> segments;
-
-    @Getter
     private final Label base;
-    @Getter
     private final String lastSegment;
 
     protected Label(final List<String> segments) {
@@ -38,33 +38,6 @@ public class Label implements Comparable<Label> {
     public static Label fromString(String string) {
         // TODO validierung auf segment zahl
         return new Label(Arrays.asList(string.split(":")));
-    }
-
-
-    @Override
-    public boolean equals(final Object object) {
-        if (this == object) {
-            return true;
-        }
-
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-
-        final Label other = (Label) object;
-
-        return new EqualsBuilder().append(value, other.value)
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37).append(value).toHashCode();
-    }
-
-    @Override
-    public String toString() {
-        return this.value;
     }
 
     @Override
